881ff8039ddd79c9a2d059f4dfeeb0beeba9e081
Y <Y@H.mshome.net>
2020-10-12 20:07:51 -0700

    First Commit....

---
+++ src/First/Git1stprogram.java
@@ -0,0 +1,14 @@
+package First;
+
+public class Git1stprogram {
+
+	public static void main(String[] args) {
+		
+
+		
+	    for (int i=0 ; i<=10; i++)
+	    {
+	    	System.out.println(i);
+	    	
+	    }
+	}}
